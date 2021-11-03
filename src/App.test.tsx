071deff05ddc8d23c27e2cb97498ed6e67b435f1
Kris Labovitch <klabovitch@athenahealth.com>
2021-11-03 16:17:16 -0400

    json db saving

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,39 @@
-// import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { isIfStatement } from 'typescript';
-import App from './App';
 
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { App } from "./App";
 
-
-describe("App",() => {
+describe("App", () => {
   it("should display a message input with an accessible label", () => {
     render(<App />);
     screen.getByLabelText("Message");
+  }); 
+
+  it("should render a Submit button with a label of 'Send'", () => {
+    render(<App />);
+    screen.getByRole("button", { name: "Send" });
+  });
+
+  it("should set aria-disabled on the Send button when the Message textarea is empty", () => {
+    render(<App />);
+    const sendButton = screen.getByRole("button", { name: "Send" });
+    expect(sendButton).toHaveAttribute("aria-disabled", "true");
+  });
+
+  it("should NOT disable the Send button when the Message textarea is populated", () => {
+    render(<App />);
+    const sendButton = screen.getByRole("button", { name: "Send" });
+    const messageInput = screen.getByLabelText("Message");
+    // userEvent.type(messageInput, "example message");
+    // expect(sendButton).toHaveAttribute("aria-disabled", "false");
+  });
+
+  it("Should clear ou the message textarea when 'Send' is clicked", () => {
+    render(<App />);
+    const sendButton = screen.getByRole("button", { name: "Send" });
+    const messageInput = screen.getByLabelText("Message");
+    userEvent.type(messageInput, "example message");
+    userEvent.click(sendButton);
+    expect(messageInput).toHaveAttribute("value", null);
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import Root from "@athena/forge/Root";
 import "@athena/forge/dist/forge.css";
 import Textarea from "@athena/forge/Textarea";
 import FormField from "@athena/forge/FormField"
-
+import { Button } from "@athena/forge";
+import { useState } from "react";
+import List from "@athena/forge/List";
+import ListItem from "@athena/forge/ListItem";
 
 const user = {
   name: "Cory",
@@ -13,11 +16,34 @@ const user = {
 const {name : myName} = user;
 
 export function App() {
+  const [message,setMessage] = useState("");
+  const [messages,setMessages] = useState<string[]>([]);
   return (
     <Root>
       <h1>Chat</h1>
-      <Form>
-        <FormField labelText="Message" id = "message" inputAs={Textarea} />
+      <List>
+      { messages.map ( (m,index) => (
+       <ListItem key={index}> <p>{m}</p> </ListItem>
+      ))}
+      </List>
+      <Form 
+        includeSubmitButton = {false} 
+          onSubmit={(event) => {
+            event.preventDefault();
+            setMessages([...messages,message])
+            setMessage("");
+          }
+        }
+        >
+        <FormField 
+          labelAlwaysAbove
+          labelText="Message" 
+          id = "message" 
+          inputAs={Textarea}
+            value={message}
+            onChange={(event) => setMessage(event.currentTarget.value)}
+        />
+        <Button type="submit" text="Send" disabled={!message} />
       </Form>
     </Root>
   );
